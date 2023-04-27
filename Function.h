8e3026c45661f567de0c2260167ee7389051350c
吉田彪 <@MSI>
2023-04-28 02:20:36 +0900

    プロジェクト ファイルを追加します。

---
+++ Function.h
@@ -0,0 +1,8 @@
+#pragma once
+//#include<Vector3AndMatrix.h>
+Matrix4x4  MakeRotateXMarix(float radian);
+Matrix4x4  MakeRotateYMarix(float radian); 
+Matrix4x4  MakeRotateZMarix(float radian);
+Matrix4x4  Multiply(const  Matrix4x4 radian, const Matrix4x4 radian2);
+
+
